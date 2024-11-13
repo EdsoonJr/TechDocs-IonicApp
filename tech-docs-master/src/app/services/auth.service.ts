9f691dd040b62633ec0324654a78667496a150b5
Edson Junior <edsonjn2013@gmail.com>
2024-11-13 12:42:25 -0300

    add user model and signup in firestore DB

--- tech-docs-master/src/app/services/auth.service.ts
+++ tech-docs-master/src/app/services/auth.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { User } from '../models/user.model'; // Importe o modelo
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(public auth: AngularFireAuth) {}
+  constructor(public auth: AngularFireAuth, private firestore: AngularFirestore) { }
 
-  loginFireAuth(value: { email: string; password: string }) {
+  loginFireAuth(value: {
+    email: string;
+    password: string
+  }) {
     return new Promise<any>((resolve, reject) => {
       this.auth.signInWithEmailAndPassword(value.email, value.password).then(
         (res) => resolve(res),
@@ -18,11 +23,23 @@ export class AuthService {
 
   registerFireAuth(value: {
     email: string;
-    password: string;
+    password: string,
+    name: string,
+    area: string
   }) {
     return new Promise<any>((resolve, reject) => {
       this.auth.createUserWithEmailAndPassword(value.email, value.password).then(
-        (res) => resolve(res),
+        async (res) => {
+          const user: User = {
+            uid: res.user?.uid || '',
+            email: value.email,
+            name: value.name,
+            area: value.area,
+            photoURL: '', // Opcional
+          };
+          await this.firestore.collection('users').doc(user.uid).set(user);
+          resolve(res);
+        },
         (error) => reject(error)
       );
     });
