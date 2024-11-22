c758b231ff6d6a59aaebd3639c6a886c9c0938cb
Giovana <gicaalopes2003@gmail.com>
2024-11-22 12:45:45 -0300

    style: starting the styling and structuring of the "folders screen"

--- tech-docs-master/src/app/pages/folder/folder.page.ts
+++ tech-docs-master/src/app/pages/folder/folder.page.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+  selector: "app-folder",
+  templateUrl: "./folder.page.html",
+  styleUrls: ["./folder.page.scss"],
 })
 export class FolderPage implements OnInit {
+  // Exemplo de dados das pastas
+  public folders = [
+    {
+      name: "Engenharia",
+      previewImageUrl: "assets/img/preview-pdf-engenharia.png", // Miniatura do PDF
+      pdfs: ["pdf1.pdf", "pdf2.pdf"],
+      isEditing: false,
+    },
+    {
+      name: "Programação",
+      previewImageUrl: "assets/img/preview-pdf-programacao.png",
+      pdfs: ["pdf1.pdf", "pdf2.pdf"],
+      isEditing: false,
+    },
+    {
+      name: "Inteligência Artificial",
+      previewImageUrl: "assets/img/preview-pdf-ia.png",
+      pdfs: ["pdf1.pdf", "pdf2.pdf"],
+      isEditing: false,
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  editFolderName(index: number) {
+    this.folders[index].isEditing = true;
   }
 
+  saveFolderName(index: number) {
+    this.folders[index].isEditing = false;
+  }
+
+  openFolder(folder: any) {
+    console.log("Abrindo pasta:", folder.name);
+  }
+  constructor() {}
+
+  ngOnInit() {}
 }
