a3b1da2014abbc071aa04dd05671eb66cd7686ed
Edson Junior <edsonjn2013@gmail.com>
2024-11-24 00:41:43 -0300

    feat-add thubnails in pdfs search

--- tech-docs-master/src/app/pages/search/search.page.ts
+++ tech-docs-master/src/app/pages/search/search.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PdfService } from '../../services/pdf.service';
+import { PdfThumbnailService } from '../../services/pdf-thumbnail.service';
 import { Pdf } from '../../models/pdfs.model';
 
 @Component({
@@ -9,10 +10,11 @@ import { Pdf } from '../../models/pdfs.model';
 })
 export class SearchPage {
   query = ''; // Termo de busca
-   field: 'title' | 'tags' = 'title'; // Campo de busca ('title', 'tags' ou 'area')
+  field: 'title' | 'tags'  = 'title'; // Campo de busca
   pdfs: Pdf[] = [];
+  thumbnails: { [key: string]: string } = {}; // Miniaturas
 
-  constructor(private pdfService: PdfService) {}
+  constructor(private pdfService: PdfService, private pdfThumbnailService: PdfThumbnailService) {}
 
   search() {
     if (!this.query) {
@@ -24,10 +26,28 @@ export class SearchPage {
       next: (results) => {
         this.pdfs = results;
         console.log('Resultados da pesquisa:', this.pdfs);
+        this.generateThumbnails();
       },
       error: (error) => {
         console.error('Erro ao buscar PDFs:', error);
       },
     });
+  }
+
+  async generateThumbnails() {
+    for (const pdf of this.pdfs) {
+      if (pdf.id && pdf.url) { // Garante que ambos existem
+        try {
+          const thumbnail = await this.pdfThumbnailService.generateThumbnail(pdf.url);
+          this.thumbnails[pdf.id] = thumbnail;
+        } catch (error) {
+          console.error(`Erro ao gerar miniatura para o PDF ${pdf.title}:`, error);
+        }
+      }
+    }
+  }
+
+  openPdf(url: string) {
+    window.open(url, '_blank');
   }
 }
