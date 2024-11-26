764e6f6982769703deca8872f04cacddebf544a6
Giovana <gicaalopes2003@gmail.com>
2024-11-26 14:58:15 -0300

    fix: turning the "my-upload" screen as a modal component

--- tech-docs-master/src/app/pages/profile/profile.page.ts
+++ tech-docs-master/src/app/pages/profile/profile.page.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IonModal, ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+  selector: "app-profile",
+  templateUrl: "./profile.page.html",
+  styleUrls: ["./profile.page.scss"],
 })
 export class ProfilePage implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
+  isModalOpen: boolean = false;
 
-  constructor() { }
+  myUploadedPDFs = [
+    {
+      id: 1,
+      title: "PDF 1",
+      description: "Alguma descrição sobre o PDF",
+      thumbnailUrl: "assets/img/pdf1-thumbnail.png",
+    },
+    {
+      id: 2,
+      title: "PDF 2",
+      description: "Alguma descrição sobre o PDF",
+      thumbnailUrl: "assets/img/pdf2-thumbnail.png",
+    },
+    {
+      id: 3,
+      title: "PDF 3",
+      description: "Alguma descrição sobre o PDF",
+      thumbnailUrl: "assets/img/pdf3-thumbnail.png",
+    },
+    {
+      id: 4,
+      title: "PDF 4",
+      description: "Alguma descrição sobre o PDF",
+      thumbnailUrl: "assets/img/pdf3-thumbnail.png",
+    },
+  ];
 
-  ngOnInit() {
+  constructor(private toastController: ToastController) {}
+
+  openModal() {
+    this.isModalOpen = true;
   }
 
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  ngOnInit() {}
+
+  async showToast(message: string, color: "success" | "warning") {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      position: "bottom",
+      color,
+    });
+    toast.present();
+  }
 }
