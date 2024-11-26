2ef51e7c83cbd5df7240ab408b4cfb10f720d364
Giovana <gicaalopes2003@gmail.com>
2024-11-26 14:13:46 -0300

    feature: upload PDF

--- tech-docs-master/src/app/pages/home/home.page.ts
+++ tech-docs-master/src/app/pages/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { OverlayEventDetail } from "@ionic/core/components";
 import { Router } from "@angular/router";
+import { IonModal, ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-home",
@@ -12,24 +14,79 @@ export class HomePage implements OnInit {
     { id: 2, title: "PDF 2", thumbnailUrl: "assets/img/pdf2-thumbnail.png" },
     { id: 3, title: "PDF 3", thumbnailUrl: "assets/img/pdf3-thumbnail.png" },
   ];
-  constructor(private router: Router) {}
+
+  @ViewChild(IonModal) modal!: IonModal;
+
+  title!: string;
+  description!: string;
+  selectedFile: File | null = null;
+  acceptTerms: boolean = false;
+  message: { title: string; description: string; fileName: string } | null = null;
+
+  constructor(private router: Router, private toastController: ToastController) {}
+
+  cancel() {
+    this.modal.dismiss(null, "cancel");
+  }
+
+  confirm() {
+    if (this.selectedFile && this.acceptTerms) {
+      const data = {
+        title: this.title,
+        description: this.description,
+        fileName: this.selectedFile.name,
+      };
+      this.modal.dismiss(data, "confirm");
+      this.showToast("PDF carregado com sucesso!");
+    }  else {
+      this.showToast("Por favor, preencha todos os campos e aceite o Termo de Condições.");
+    }
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<
+      OverlayEventDetail<{ title: string; description: string; fileName: string }>
+    >;
+    if (ev.detail.role === "confirm") {
+      this.message = ev.detail.data || null;
+    }
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
+
+  isFormValid(): boolean {
+    return !!this.title && !!this.description && !!this.selectedFile && this.acceptTerms;
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      position: "bottom",
+      color: "success",
+    });
+    toast.present();
+  }
 
   ngOnInit() {
-   // Carregar os PDFs favoritados a partir da API ou banco de dados.
+    // Carregar os PDFs favoritados a partir da API ou banco de dados.
   }
 
   openPDF(pdf: any) {
     console.log("Abrir PDF:", pdf);
-    // Implementar lógica para abrir o PDF.
   }
 
   navigateToFavorites() {
     console.log("Ir para página de Favoritos");
-    // Implementar lógica para navegar à página de Favoritos.
   }
 
   goToProfile() {
-    console.log('Navegando para a página de perfil...');
-    this.router.navigate(['/profile']); // Altere para a rota da página de perfil
+    console.log("Navegando para a página de perfil...");
+    this.router.navigate(["/profile"]);
   }
 }
